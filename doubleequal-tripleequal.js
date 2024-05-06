2ba324c13725f757c94c725067acef21bfbb6c5e
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-06 16:16:40 +0500

    difference between == and ===

---
+++ doubleequal-tripleequal.js
@@ -0,0 +1,15 @@
+var x=4
+var y='4'
+
+
+//  == only compares values
+console.log(x==y)
+
+
+// === compares both value and datatype
+console.log(x===y)
+
+
+// Javascript concatinate string with number
+console.log('10'+10)
+console.log(10+'10')
