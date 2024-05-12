b61ed6d8261c937c436b7ab217639be7202e4316
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-12 21:04:33 +0500

    celcius to fahrenheit & fahrenheit to celcius

---
+++ celsius-to-fahrenheit.js
@@ -0,0 +1,15 @@
+// Celsius to fahrenheit
+
+const celsius = 19;
+
+const conversionToFahrenheit = celsius*1.8+32
+
+console.log(conversionToFahrenheit)
+
+// Fahrenheit to celcius
+
+const fahrenheit = 66.2;
+
+const convertionToFahrenheit = 5/9*(fahrenheit-32)
+
+console.log(convertionToFahrenheit)
