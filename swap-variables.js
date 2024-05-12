289b30bc0fc1be0ce61e4bdb7840275cdf663b6c
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-12 17:30:19 +0500

    swaping variables with & without third variable

---
+++ swap-variables.js
@@ -0,0 +1,21 @@
+
+let x=2;
+let y=3;
+
+// Swap values of two variables using 3rd variable
+let temp = x;
+x=y;
+y=temp;
+
+console.log(`x = ${x} and y = ${y}`)
+
+// swaping without using 3rd variable
+
+
+let a=2;
+let b=3;
+a=a+b    // 2+3 = 5
+b=a-b    // 5-3 = 2
+a=a-b    // 5-2 = 3
+
+console.log(`Swap without 3rd variable  a = ${a} and b = ${b}`)
